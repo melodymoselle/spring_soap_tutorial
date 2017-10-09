8cb1c0ef519b276f8d91569ae24420ca842c44c1
Melody Moselle <mmoselle@vegaspbs.org>
2017-10-09 09:46:48 -0700

    finished 'creating the client'

---
+++ src/test/java/com/melodymoselle/ws/client/HelloWorldClient.java
@@ -0,0 +1,34 @@
+package com.melodymoselle.ws.client;
+
+import com.melodymoselle.types.helloworld.Greeting;
+import com.melodymoselle.types.helloworld.ObjectFactory;
+import com.melodymoselle.types.helloworld.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+@Component
+public class HelloWorldClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(HelloWorldClient.class);
+
+    @Autowired
+    private WebServiceTemplate webServiceTemplate;
+
+    public String sayHello(String firstName, String lastName) {
+        ObjectFactory factory = new ObjectFactory();
+        Person person = factory.createPerson();
+
+        person.setFirstName(firstName);
+        person.setLastName(lastName);
+
+        LOGGER.info("Client sending person[firstName={},lastName={}]", person.getFirstName(), person.getLastName());
+
+        Greeting greeting = (Greeting) webServiceTemplate.marshalSendAndReceive(person);
+
+        LOGGER.info("Client received greeting='{}'", greeting.getGreeting());
+        return greeting.getGreeting();
+    }
+}
