781c9204c262c12e41b5aa8810525dfd279e974d
Melody Moselle <mmoselle@vegaspbs.org>
2017-10-09 14:56:51 -0700

    working on tests

---
+++ src/test/java/com/melodymoselle/ws/SpringWsApplicationTests.java
@@ -0,0 +1,25 @@
+package com.melodymoselle.ws;
+
+import com.melodymoselle.ws.client.HelloWorldClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
+public class SpringWsApplicationTests {
+
+    @Autowired
+    private HelloWorldClient helloWorldClient;
+
+    @Test
+    public void testSayHello() {
+        assertThat(helloWorldClient.sayHello("John", "Doe")).isEqualTo("Hello John Doe!");
+
+    }
+}
